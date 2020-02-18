e19e7b876e62d3d65dcfd61486c4e8fe7f53133c
Ionut Moraru <morion4000@gmail.com>
2020-02-18 09:28:20 +0200

    Update Storage.js

--- src/components/Storage.js
+++ src/components/Storage.js
@@ -19,24 +19,8 @@ class Storage {
     compounds: true
 }, {
     type: Storage.TYPES.SERVICE,
-    name: 'Petrolube Maintenance',
+    name: 'Petro Maintenance',
     amount: 100
-  }, {
-    type: Storage.TYPES.SERVICE,
-    name: 'Dermod Wood Maintenance',
-    amount: 10
-  }, {
-    type: Storage.TYPES.SERVICE,
-    name: 'WebDollar Maintenance',
-    amount: 90
-  }, {
-    type: Storage.TYPES.SERVICE,
-    name: 'WebDollar Tip Bot',
-    amount: 20
-  }, {
-    type: Storage.TYPES.SERVICE,
-    name: 'Hostero Dashboard',
-    amount: 10
   }, {
     type: Storage.TYPES.SERVICE,
     name: 'Hostero Bootstrap',
@@ -65,10 +49,6 @@ class Storage {
     type: Storage.TYPES.EXPENSE,
     name: 'Misc',
     amount: 100
-  }, {
-    type: Storage.TYPES.EXPENSE,
-    name: 'Taxes',
-    amount: 250
   }];
 
   static getItems(type = null) {
